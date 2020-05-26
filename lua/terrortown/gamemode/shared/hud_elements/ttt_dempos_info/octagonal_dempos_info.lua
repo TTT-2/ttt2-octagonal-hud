a7c97970a9257dec6db76bbabf35658c4061dbc4
Leon Masopust <leon.masopust@student.hpi.de>
2020-05-26 21:04:20 +0200

    dempos hudelement is only visible if the addon is installed

--- lua/terrortown/gamemode/shared/hud_elements/ttt_dempos_info/octagonal_dempos_info.lua
+++ lua/terrortown/gamemode/shared/hud_elements/ttt_dempos_info/octagonal_dempos_info.lua
@@ -14,7 +14,6 @@ if CLIENT then -- CLIENT
 	}
 
 	local barFont = "OctagonalBar"
-
 	local gap = 5
 
 	function HUDELEMENT:PreInitialize()
@@ -59,51 +58,9 @@ if CLIENT then -- CLIENT
 
 	function HUDELEMENT:ShouldDraw()
 		local c = LocalPlayer()
-		return IsValid(c) and c:GetNWBool("DPActive") and c:GetNWBool("DPControlling") or HUDEditor.IsEditing
+		return IsValid(c) and c:GetNWBool("DPActive") and c:GetNWBool("DPControlling") or HUDEditor.IsEditing and items.IsItem("item_demonic_possession")
 	end
 	-- parameter overwrites end
-
-	function HUDELEMENT:DrawSupersheep(x, y, w, h, fontColor, supersheep)
-		local rx, ry = x, y
-		local bw, bh = w, self.barheight
-
-		--draw timer
-		local timeLeft = math.max(math.Truncate(30 - (CurTime() - supersheep.SheepStartTime),0) + 1, 0)
-		local timerX = x + w * 0.5 - self.timersize * 0.5 - self.pad
-		local timerY = y - bh
-		self:DrawBg(timerX, timerY, self.timersize + 2 * self.pad, bh, self.healthBarColor)
-		draw.AdvancedText(tostring(timeLeft), timerFont, timerX + self.pad + self.timersize * 0.5, timerY + bh * 0.5, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, false, self.scale)
-		--drawing interpolation bar
-		self:DrawBg(timerX, timerY, self.pad, bh, self.darkOverlayColor)
-		self:DrawBg(timerX + self.timersize + self.pad, timerY, self.pad, bh, self.darkOverlayColor)
-
-		--draw boost bar
-		self:DrawBar(rx, ry, bw, bh, self.ammoBarColor, supersheep.Boost / supersheep.MaxBoost, self.scale)
-		draw.AdvancedText("R - Boost", barFont, rx + bw * 0.5, ry + bh * 0.5, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, false, self.scale)
-
-		ry = ry + bh
-
-		--draw minify bar
-		local text = supersheep.Minified and "Right Click - Magnify" or "Right Click - Minify"
-		self:DrawBar(rx, ry, bw, bh, self.extraBarColor, math.min(CurTime() - supersheep.LastSizeChange, 5.0) / 5.0, self.scale)
-		draw.AdvancedText(text, barFont, rx + bw * 0.5, ry + bh * 0.5, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, false, self.scale)
-
-	end
-
-	function HUDELEMENT:DrawObserverSheep(x, y, w, h, fontColor, supersheep)
-		local rx, ry = x, y
-		local bw, bh = w, self.barheight
-		--draw boost bar
-		self:DrawBar(rx, ry, bw, bh, self.ammoBarColor, supersheep.Boost / supersheep.MaxBoost, self.scale)
-		draw.AdvancedText("R - Boost", barFont, rx + bw * 0.5, ry + bh * 0.5, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, false, self.scale)
-
-		ry = ry + bh
-
-		--draw minify bar
-		self:DrawBar(rx, ry, bw, bh, self.extraBarColor, math.min(CurTime() - supersheep.LastTrack, 1) / 1.0, self.scale)
-		draw.AdvancedText("Left Click - Track", barFont, rx + bw * 0.5, ry + bh * 0.5, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, false, self.scale)
-
-	end
 
 	function HUDELEMENT:Draw()
 		local client = LocalPlayer()

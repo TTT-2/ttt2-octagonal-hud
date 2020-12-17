a7ab9f93784c4a65497bc8550985987c0e58f16c
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2020-12-17 14:04:41 +0100

    Increased Scalability
    Beside the perfect text alignment this HUD now is fully scalable with scale and also changeable in width, height and position.

--- lua/terrortown/gamemode/shared/hud_elements/ttt_demonicsheep_info/octagonal_demonicsheep_info.lua
+++ lua/terrortown/gamemode/shared/hud_elements/ttt_demonicsheep_info/octagonal_demonicsheep_info.lua
@@ -17,14 +17,13 @@ if CLIENT then -- CLIENT
 	local interpColor = controlColor1
 	local interpCount = 1
 	local barHeight = 26
-	local pad = 15
-	local spaceBar = 7
+	local pad = 7
 	local timersize = 42
 
 	local const_defaults = {
 		basepos = {x = 0, y = 0},
-		size = {w = 340, h = 2 * barHeight + 3 * spaceBar},
-		minsize = {w = 340, h = 2 * barHeight + 3 * spaceBar}
+		size = {w = 340, h = 2 * barHeight + 3 * pad},
+		minsize = {w = 340, h = 2 * barHeight + 3 * pad}
 	}
 
 	function HUDELEMENT:PreInitialize()
@@ -44,7 +43,6 @@ if CLIENT then -- CLIENT
 		self.basecolor = self:GetHUDBasecolor()
 		self.barHeight = barHeight * self.scale
 		self.pad = pad * self.scale
-		self.spaceBar = spaceBar * self.scale
 		self.timersize = timersize * self.scale
 
 		BaseClass.Initialize(self)
@@ -55,7 +53,6 @@ if CLIENT then -- CLIENT
 		self.scale = self:GetHUDScale()
 		self.barHeight = barHeight * self.scale
 		self.pad = pad * self.scale
-		self.spaceBar = spaceBar * self.scale
 		self.timersize = timersize * self.scale
 
 		BaseClass.PerformLayout(self)
@@ -102,28 +99,33 @@ if CLIENT then -- CLIENT
 
 	function HUDELEMENT:Drawdemonicsheep(x, y, w, h, fontColor, client)
 		local demonicsheep = client:GetActiveWeapon()
+		if not IsValid(demonicsheep) then return end
+
 		local ent = demonicsheep:GetdemonicSheepEnt()
 
 		local health = 100
 		local maxHealth = 100
+		local elementSize = 2
 
 		if IsValid(ent) then
 			health = ent:Health()
 			maxHealth = ent:GetMaxHealth()
 		end
 
-		local rx, ry = x + self.spaceBar, y + self.spaceBar
-		local bw, bh = w - 2 * self.spaceBar, 26 * self.scale
+
+		local rx, ry = x + self.pad, y + self.pad
+		local bw, bh = w - 2 * self.pad, (h - ((elementSize + 1) * self.pad) ) / elementSize
 
 		--draw Health bar
-		self:DrawBar(rx, ry, bw, bh, healthColor, health / maxHealth, self.scale)
-		draw.AdvancedText("Health: ", "OctagonalBar", rx + self.spaceBar, ry + 1 * self.scale, fontColor, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT, true, self.scale)
-		draw.AdvancedText(tostring(health .. "/" .. tostring(maxHealth)), "OctagonalBar", rx + bw * 0.5, ry + 1 * self.scale, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_LEFT, true, self.scale)
+		self:DrawBar(rx, ry , bw, bh, healthColor, health / maxHealth, self.scale)
+		draw.AdvancedText("Health: ", "OctagonalBar", rx + self.pad, ry + bh / 2, fontColor, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, true, self.scale)
+		draw.AdvancedText(tostring(health .. "/" .. tostring(maxHealth)), "OctagonalBar", rx + bw * 0.55, ry + bh / 2, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, true, self.scale) -- shouldnt be bw*0.55 but same position as ControlMode
 
-		ry = ry + self.barHeight + self.spaceBar
+		ry = ry + bh + self.pad
 
 		local controlState = demonicsheep:GetcurrentControlType()
-		local maxStates = #demonicsheep.availableControls
+		local availableControls = demonicsheep.availableControls or {[controlState] = {"Missing Control",1}}
+		local maxStates = #availableControls
 
 		if interpCount ~= controlState then
 			interpCount = controlState
@@ -131,14 +133,14 @@ if CLIENT then -- CLIENT
 		end
 
 		--draw ControlMode bar
-		--TODO: Align central controlTextCenter between controlTextLeft and controlTextRight (currently a little bit overlapping for some controlModes)
+		--TODO: Align central controlTextCenter between controlTextLeft and controlTextRight for perfect scalability (currently just a bit shifted to the right so overlapping doesnt occur)
 		local controlTextLeft = "Control:"
-		local controlTextCenter = demonicsheep.availableControls[controlState][1]
+		local controlTextCenter = availableControls[controlState][1]
 		local controlTextRight = "(" .. tostring(controlState) .. "/" .. tostring(maxStates) .. ")"
-		self:DrawBar(rx, ry, bw, bh, interpColor, 1, self.scale) -- old Bar with progress p=(controlState - 1) / (maxStates - 1)
-		draw.AdvancedText(controlTextLeft, "OctagonalBar", rx + self.spaceBar, ry + 1 * self.scale, fontColor, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT, true, self.scale)
-		draw.AdvancedText(controlTextCenter, "OctagonalBar", rx + bw * 0.5, ry + 1 * self.scale, fontColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_LEFT, true, self.scale)
-		draw.AdvancedText(controlTextRight, "OctagonalBar", rx + bw - self.spaceBar, ry + 1 * self.scale, fontColor, TEXT_ALIGN_RIGHT, TEXT_ALIGN_LEFT, true, self.scale)
+		self:DrawBar(rx, ry, bw, bh, interpColor, 1, self.scale)
+		draw.AdvancedText(controlTextLeft, "OctagonalBar", rx + self.pad, ry + bh / 2, fontColor, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, true, self.scale)
+		draw.AdvancedText(controlTextCenter, "OctagonalBar", rx + bw * 0.55, ry + bh / 2, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, true, self.scale) -- shouldnt be bw*0.55 but bw*0.5 or just centered between both texts
+		draw.AdvancedText(controlTextRight, "OctagonalBar", rx + bw - self.pad, ry + bh / 2, fontColor, TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER, true, self.scale)
 	end
 
 	function HUDELEMENT:ColorInterp(Color1, Color2, step, maxSteps)
